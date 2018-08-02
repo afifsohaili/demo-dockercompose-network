67d4ba34bae89634a71dbd90ecaab320732ae5a6
Afif Sohaili <afifnajib@gmail.com>
2018-08-02 10:19:52 +0800

    Add frontend, basic page to query todos from backend

---
+++ frontend/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="container">
+    TODOS:
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        {{todo.description}}
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  asyncData: async function () {
+    const { data: { data } } = await axios.get(process.env.API_URL + '/todos');
+    return { todos: data }
+  },
+  data () {
+    return { todos: [] }
+  }
+}
+</script>
+
+<style>
+body {
+  padding: 1rem;
+}
+</style>
